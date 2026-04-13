045d78e331f279467ed5b804e93b3725700cb26a
ermal19-BS7 <ermal.berisha6@student.uni-pr.edu>
2026-04-13 12:28:51 +0200

    Commit 1 – Server bazë + komunikim
    krijimi i serverit baze dhe disa komandve per komunikim

--- UDP_Server1.cpp
+++ UDP_Server1.cpp
@@ -1,1 +1,38 @@
+#include <iostream>
+#include <WS2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+using namespace std;
 
+int SERVER_PORT = 54000;
+
+int main()
+{
+    WSADATA data;
+    WSAStartup(MAKEWORD(2, 2), &data);
+
+    SOCKET in = socket(AF_INET, SOCK_DGRAM, 0);
+
+    sockaddr_in serverHint{};
+    serverHint.sin_family = AF_INET;
+    serverHint.sin_port = htons(SERVER_PORT);
+    serverHint.sin_addr.s_addr = INADDR_ANY;
+
+    bind(in, (sockaddr*)&serverHint, sizeof(serverHint));
+
+    sockaddr_in client{};
+    int clientLength = sizeof(client);
+    char buf[4096];
+
+    while (true)
+    {
+        int bytesIn = recvfrom(in, buf, sizeof(buf), 0,
+            (sockaddr*)&client, &clientLength);
+
+        if (bytesIn == SOCKET_ERROR) continue;
+
+        cout << "Received: " << string(buf, bytesIn) << endl;
+
+        sendto(in, buf, bytesIn, 0,
+            (sockaddr*)&client, clientLength);
+    }
+}
