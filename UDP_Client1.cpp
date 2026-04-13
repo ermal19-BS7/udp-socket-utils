5d876ac07e9446813aab908e4afd3aa1d07d6ddb
erisa-muharremi <erisa.muharremi@student.uni-pr.edu>
2026-04-13 14:08:01 +0200

    Client + sendCommand

--- UDP_Client1.cpp
+++ UDP_Client1.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <WS2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+using namespace std;
 
+string sendCommand(SOCKET sock, sockaddr_in& server, string cmd)
+{
+    sendto(sock, cmd.c_str(), cmd.size() + 1, 0,
+        (sockaddr*)&server, sizeof(server));
+
+    char buf[8192];
+    int len = sizeof(server);
+
+    int bytes = recvfrom(sock, buf, 8192, 0,
+        (sockaddr*)&server, &len);
+
+    return string(buf, bytes);
